ed5e1d13ddf179d435f577164f6f2a9bccb753b9
arunsanna <arun@arunlabs.org>
2015-10-11 15:24:16 -0700

    Creating running files for the Instances

---
+++ start-instance1.sh
@@ -0,0 +1,3 @@
+export CATALINA_BASE=/usr/local/java/apache-tomcat-8.0.27/instance1
+$CATALINA_HOME/bin/startup.sh
+
---
+++ start-instance2.sh
@@ -0,0 +1,2 @@
+export CATALINA_BASE=/usr/local/java/apache-tomcat-8.0.27/instance2
+$CATALINA_HOME/bin/startup.sh
