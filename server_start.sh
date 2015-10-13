e403aa4d3556990ad9e32761c70bad79a86b8c3b
arunsanna <arun@arunlabs.org>
2015-10-13 00:19:21 -0700

    complete server script
    not tested

---
+++ server_start.sh
@@ -0,0 +1,4 @@
+sh /usr/local/java/apache-tomcat-8.0.27/bin/startup.sh
+sh /usr/local/java/apache-tomcat-8.0.27/start-instance1.sh
+sh /usr/local/java/apache-tomcat-8.0.27/start-instance2.sh
+sudo service apache2 start
